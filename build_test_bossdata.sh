5f92d3ae17f2ea1e87840f93e27f932ab7651a40
yellows8 <yellows8@users.noreply.github.com>
2016-12-25 16:46:42 -0500

    Switched to uppercase regionids for build_test_bossdata.sh.

--- build_test_bossdata.sh
+++ build_test_bossdata.sh
@@ -5,10 +5,10 @@
 params="--input=bossinput --build --nsdataid=0x58584148"
 echo -n "Hello" > bossinput
 mkdir web/boss
-bosstool --output=web/boss/bossdata_jpn --programID=0004003000008202 $params $1
-bosstool --output=web/boss/bossdata_usa --programID=0004003000008f02 $params $1
-bosstool --output=web/boss/bossdata_eur --programID=0004003000009802 $params $1
-bosstool --output=web/boss/bossdata_chn --programID=000400300000A102 $params $1
-bosstool --output=web/boss/bossdata_kor --programID=000400300000A902 $params $1
-bosstool --output=web/boss/bossdata_twn --programID=000400300000B102 $params $1
-bosstool --output=web/boss/bossdata_usa --programID=0004003000008f02 $params $1
+bosstool --output=web/boss/bossdata_JPN --programID=0004003000008202 $params $1
+bosstool --output=web/boss/bossdata_USA --programID=0004003000008f02 $params $1
+bosstool --output=web/boss/bossdata_EUR --programID=0004003000009802 $params $1
+bosstool --output=web/boss/bossdata_CHN --programID=000400300000A102 $params $1
+bosstool --output=web/boss/bossdata_KOR --programID=000400300000A902 $params $1
+bosstool --output=web/boss/bossdata_TWN --programID=000400300000B102 $params $1
+bosstool --output=web/boss/bossdata_USA --programID=0004003000008f02 $params $1
